e34c59c15bde819b8e6a4a5d6402cfb643a185f9
Kaley <kaleidox@comroid.org>
2024-12-24 04:36:13 +0100

    add preparation tasks to modsuite buildscript

--- gradle/src/main/groovy/com/ampznetwork/modsuite/buildscripts/ModSuiteBuildHelper.groovy
+++ gradle/src/main/groovy/com/ampznetwork/modsuite/buildscripts/ModSuiteBuildHelper.groovy
@@ -1,16 +1,10 @@
 package com.ampznetwork.modsuite.buildscripts
 
-import com.ampznetwork.modsuite.buildscripts.task.BuildArtifactsTask
-import com.ampznetwork.modsuite.buildscripts.task.DownloadRequirementsTask
-import com.ampznetwork.modsuite.buildscripts.task.PublishArtifactsTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class ModSuiteBuildHelper implements Plugin<Project> {
     @Override
     void apply(Project project) {
-        project.tasks.register('downloadRequirements', DownloadRequirementsTask)
-        project.tasks.register('buildArtifactsTask', BuildArtifactsTask)
-        project.tasks.register('publishArtifactsTask', PublishArtifactsTask)
     }
 }
