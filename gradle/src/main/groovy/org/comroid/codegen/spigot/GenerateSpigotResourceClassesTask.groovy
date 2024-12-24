35018d02542fe40b55577ef23e855852c2048af2
Kaley <kaleidox@comroid.org>
2024-12-24 19:52:04 +0100

    compile fix

--- gradle/src/main/groovy/org/comroid/codegen/spigot/GenerateSpigotResourceClassesTask.groovy
+++ gradle/src/main/groovy/org/comroid/codegen/spigot/GenerateSpigotResourceClassesTask.groovy
@@ -60,7 +60,7 @@ abstract class GenerateSpigotResourceClassesTask extends DefaultTask {
 
                 java.beginClass().kind(ElementKind.ENUM).name("LoadTime").and()
                         .writeIndent()
-                        .writeTokenList("", "", List.of("STARTUP", "POSTWORLD"), Function.identity(), ",", false)
+                        .writeTokenList("", "", List.of("STARTUP", "POSTWORLD"), Function.identity(), ",")
                         .writeLineTerminator()
                         .end()
             } catch (Throwable t) {
