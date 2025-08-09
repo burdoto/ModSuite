0bcdc3f83083ef30b927121747e70ddee68d3ece
Kaley <kaleidox@comroid.org>
2025-08-09 12:23:25 +0200

    update buildscript to include test results

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/bash
 
 git fetch --all && git reset --hard origin/main
+
+# prepare buildscript dependency
 (cd japi && ./gradlew publishToMavenLocal && echo "japi was built and published locally") || (echo "Could not build japi" && exit 1)
-./gradlew clean prepareSpigotTest prepareFabricTest || echo "Could not build ModSuite Files"
+
+# cleanup & run tests to show results of current build
+./gradlew clean test
+
+# try to build
+./gradlew prepareSpigotTest prepareFabricTest || (echo "Could not build ModSuite Files" && exit 1)
