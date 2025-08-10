d9f7188c25078785086df1ad0e6b52898ed4c5c7
Kaley <kaleidox@comroid.org>
2025-08-10 20:01:03 +0200

    pull from distinct branch

--- pull.sh
+++ pull.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
-git pull
-(cd japi && git pull)
-(cd LibMod && git pull)
-(cd BanMod && git pull)
-(cd ChatMod && git pull)
-(cd ChatMod/thirdparty/AurionChat && git pull)
-(cd WorldMod && git pull)
+git pull origin main
+(cd japi && git pull origin main)
+(cd LibMod && git pull origin main)
+(cd BanMod && git pull origin main)
+(cd WorldMod && git pull origin main)
+(cd ChatMod && git pull origin main)
+(cd ChatMod/thirdparty/AurionChat && git pull origin patch-1)
